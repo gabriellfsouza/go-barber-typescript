a457dfadd588b3646351c9322cee615ad95ad6d1
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-04-19 09:09:57 -0300

    chore: first service class, Single responsability and Dependency Inversion

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  const message = 'Hello World';
-  return response.json({ message });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
