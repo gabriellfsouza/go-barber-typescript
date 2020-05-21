b09fdf5fe0eb4f7623a2fd597285cf06b2c6ac5f
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-05-21 07:11:41 -0300

    first test script

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import CreateAppointmentService from './CreateAppointmentService';
+import { startOfHour } from 'date-fns';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const fakeData = { date: new Date(), provider_id: '123123123' };
+
+    const appointment = await createAppointment.execute(fakeData);
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe(fakeData.provider_id);
+    expect(appointment.date.toISOString()).toBe(
+      startOfHour(fakeData.date).toISOString(),
+    );
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
